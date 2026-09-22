4b9f65f369e4b29843c8357a6a98ae2e082cba0c
TomEd01 <l21530369@cancun.tecnm.mx>
2026-09-22 12:17:34 -0500

    Implementacion de serializador para el registro de usuarios con google

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,24 +1,9 @@
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
 
-# get_user_model() manda llamar al AUTH_USER_MODEL del settings.py de forma segura
-Usuario = get_user_model()
-
-class RegistroUsuarioSerializer(serializers.ModelSerializer):
-    # CWrite_only=True asegura que la contraseña no viaje al frontend
-    password = serializers.CharField(
-        max_length=128,
-        write_only=True,
-        style={'input_type': 'password'} # Oculta el texto en la interfaz navegable de DRF
-    )
-
-    class Meta:
-        # Vincula el serializador con tu modelo exacto
-        model = Usuario
-        fields = ['email', 'nombre_completo', 'password', 'rol']
-
-    def create(self, validated_data):
-        # Usa el mánager para encriptar la contraseña correctamente
-        # **validated_data desempaqueta el diccionario como argumentos clave-valor
-        user = Usuario.objects.create_user(**validated_data)
-        return user
+class GoogleAuthSerializer(serializers.Serializer):
+    # Flutter nos mandará un token cifrado kilométrico
+    id_token = serializers.CharField(write_only=True)
+    
+    # El rol es opcional porque, si el usuario ya existe y solo está iniciando sesión,
+    # no necesita mandarnos su rol otra vez. Solo lo manda en su primer registro.
+    rol = serializers.CharField(max_length=50, required=False)
